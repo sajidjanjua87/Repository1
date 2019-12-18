225117633df3e352c85ebd4d91b9413e3c0a7540
sajid <sajidjanjua87@gmail.com>
2019-12-19 06:44:03 +1100

    adding my task

--- js/app.js
+++ js/app.js
@@ -1,127 +1,105 @@
 'use strict';
+var score=0;
 
 //welcome message
 alert('Welcome to My Website');
 
 // User name prompt
 var name =prompt('May I know your name please!');
+
+//confirmation of name entry
+while(!name){
+    name = prompt('Please Enter your name');
+}
+
+//User name display
  alert('Hello ' + name + '! Welcome To My Website');
- console.log('Name', name)
 
- //questions
-//question 1
-var love = prompt('Do you love coding?');
+//questions 1-5
+//Questions, positive reply, negative reply
+var questions = [
+    ['Do you love coding?', 'ofcourse, you do!', 'pity' ],
+    ['Do you like swimming?', 'A great sports indeed!!', 'Yeah it can be quite exausting' ],
+    ['Do you drink coffee?', 'Its delicious!!!', 'You gotta be kidding me!'],
+    ['Can you teach a cat to talk?', 'You should be a trainer', 'cats never learn'],
+    ['Do you believe global warming is a hoax?', 'You should check the latest researches', 'Yet there is nothing much we are doing to avoid it!']
+]
 
-love = love.toLowerCase();
-
-switch (love){
+//For loop feeds one entry at a time from questions array (5 entries) to currentquestion array (3 entries)
+//currentquestion array entries are fed to currentprompt, positivereply and negativereply vars respectively
+for(var i=0; i<5; i++){
+var currentquestion=questions[i];
+var currentprompt= prompt(currentquestion[0]);
+currentprompt=currentprompt.toLowerCase();
+var positivereply=currentquestion[1];
+var negativereply=currentquestion[2];
+switch(currentprompt){
     case 'yes':
     case 'y':
-        alert('ofcourse, you do!');
-        console.log('You selected yes for question 1');
+        alert(positivereply);
+        score=score+1;
         break;
     case 'no':
     case 'n':
-        alert('pity');
-        console.log('You selected no for question 1');
+        alert(negativereply);
+        score=score+1;
         break;
     default:
         alert('Sorry! I didn\'t get that');
-        console.log('Your answer for question 1 is not valid');
+}}
+
+//Question 6
+var guessnumber=prompt('Guess a number between 1 to 10, You have 4 chances');
+for(var x=4; x>0; x--){
+    var y=x-1;
+    if (guessnumber == 5)
+    {alert('You guessed it right, the number is 5');
+    score=score+1;
+    break;}
+    else if (guessnumber<5)
+        alert('Your guess is lower');
+    else if (guessnumber > 5)
+        alert('Your guess is higher');
+    if(y>0)
+    guessnumber=prompt('You have ' + (y) + ' chances left');
+    }
+if (y==0 && guessnumber!=5)
+    alert('Sorry you ran out of chances, the number is 5');
+
+//Question 7, Multiple entries in an array
+var multi=[7, 15, 27, 36,45];
+var guessmulti=prompt('Guess a number between 1 to 50 with multiple possibilities, you have 6 chances');
+console.log(multi)
+for(var m=6; m>0; m--){
+    var n=m-1;
+    for(var c=0; c<5; c++){
+        if (guessmulti==multi[c]){
+            alert('You guessed it right, ' + guessmulti + '. All the possible numbers are 7, 15, 27, 36, 45');
+            score=score+1;
+            var flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    break;
+    if(n>0)
+    guessmulti=prompt('Your have ' + n + ' chances left');
 }
+if(n==0 && flag!=1)
+    {alert('Sorry you ran out of chances, the numbers are 7, 15, 27, 36, 45');}
 
-//Question 2
-var swim = prompt('Do you like swimming?');
-
-swim = swim.toLowerCase();
-
-switch(swim){
-    case 'yes':
-    case 'y':
-        alert('A great sports indeed!!');
-        console.log('You selected yes for question 2');
-        break;
-    case 'no':
-    case 'n':
-        alert('You need to workout fatty :)');
-        console.log('You selected no for question 2');
-        break;
-    default:
-        alert('Sorry! I didn\'t get that');
-        console.log('Your answer for question 2 is not valid');
-}
-
-//Question 3
-var coffee = prompt('Do you drink coffee?');
-
-coffee = coffee.toLowerCase();
-
-switch(coffee){
-    case 'yes':
-    case 'y':
-        alert('Its delicious!!!');
-        console.log('You selected yes for question 3');
-        break;
-    case 'no':
-    case 'n':
-        alert('You gotta be kidding me!');
-        console.log('You selected no for question 3');
-        break;
-    default:
-        alert('Sorry! I didn\'t get that');
-        console.log('Your answer for question 3 is not valid');
-}
-
-//Question 4
-var cat= prompt('Can you teach a cat to talk?');
-
-cat=cat.toLowerCase();
-
-switch(cat){
-    case 'yes':
-    case 'y':
-        alert('You should be a trainer');
-        console.log('You selected yes for question 4');
-        break;
-    case 'no':
-    case 'n':
-        alert('cats never learn');
-        console.log('You selected no for question 4');
-        break;
-    default:
-        alert('Sorry! I didn\'t get that');
-        console.log('Your answer for question 4 is not valid');
-}
-
-//Question 5
-var global= prompt('Do you believe global warming is a hoax?');
-
-global=global.toLowerCase();
-
-switch(global){
-    case 'yes':
-    case 'y':
-        alert('You should check the latest researches');
-        console.log('You selected yes for question 5');
-        break;
-    case 'no':
-    case 'n':
-        alert('Yet there is nothing much we are doing to avoid it!');
-        console.log('You selected no for question 5');
-        break;
-    default:
-        alert('Sorry! I didn\'t get that');
-        console.log('Your answer for question 5 is not valid');
-}
+//score
+alert('Your score is ' + score + ' out of 7');
 
 //Farewell
 alert('Thanks for your time ' + name + ', Have a nice day!');
 
 window.onload = function() {
 document.getElementById('name').innerHTML= name;
-document.getElementById('Ans1').innerHTML= love;
-document.getElementById('Ans2').innerHTML= swim;
-document.getElementById('Ans3').innerHTML= coffee;
-document.getElementById('Ans4').innerHTML= cat;
-document.getElementById('Ans5').innerHTML= global;
+document.getElementById('Ans1').innerHTML= displayreply[0];
+console.log('Ans1', displayreply[0]);
+document.getElementById('Ans2').innerHTML= displayreply[1];
+document.getElementById('Ans3').innerHTML= displayreply[2];
+document.getElementById('Ans4').innerHTML= displayreply[3];
+document.getElementById('Ans5').innerHTML= displayreply[4];
 }
